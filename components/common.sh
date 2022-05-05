4b33dfa09c4f66f8fcb1aa54f11241b55d2a3acf
kiran <kirangummadi1985@gmail.com>
2022-05-05 22:13:32 +0530

    Updating roboshop-setup-scripts

---
+++ components/common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+checkRootUser() {
+  USER_ID=$(id -u)
+
+  if [ "$USER_ID" -ne "0" ]; then
+    echo -e "\e[31mYou are suppose to be running this script as sudo or root user\e[0m"
+    exit 1
+  fi
+}
+
+
+statusCheck() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    echo "Check the error log in ${LOG_FILE}"
+    exit 1
+  fi
+}
+
