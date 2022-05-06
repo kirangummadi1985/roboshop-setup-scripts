97068ecdcfeacf11e6d802ef81aecd8a3de4c98b
kiran <kirangummadi1985@gmail.com>
2022-05-06 13:08:55 +0530

    ec2-create-script

--- create-ec2.sh
+++ create-ec2.sh
@@ -12,7 +12,7 @@ fi
 
 NAME=$1
 
-aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${NAME} Name=state,Values=active --output table | grep InstanceId &>/dev/null
+aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${NAME} Name=state,Values=active --output table | grep InstanceId
 if [ $? -eq 0 ]; then
   echo "Instance Already exists"
   exit 0
@@ -20,7 +20,7 @@ fi
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table  | grep ImageId | awk '{print $4}')
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" --security-group-ids sg-052da698a233eaebe &>/dev/null
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" --security-group-ids sg-052da698a233eaebe
 echo EC2 Instance Created
 
 sleep 30
