008db93b8767ef43b92d95620dbbc58767dcd409
kiran <kirangummadi1985@gmail.com>
2022-05-06 13:16:02 +0530

    Updating roboshop-setup-scripts

--- create-ec2.sh
+++ create-ec2.sh
@@ -12,7 +12,7 @@ fi
 
 NAME=$1
 
-aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${NAME} Name=state,Values=active --output table | grep InstanceId
+aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${NAME} Name=state,Values=active --output table | grep InstanceId &>/dev/null
 if [ $? -eq 0 ]; then
   echo "Instance Already exists"
   exit 0
@@ -20,7 +20,7 @@ fi
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table  | grep ImageId | awk '{print $4}')
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" --security-group-ids sg-052da698a233eaebe
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" --security-group-ids sg-0672afdd5f69d31fc &>/dev/null
 echo EC2 Instance Created
 
 sleep 30
@@ -29,5 +29,5 @@ INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Va
 IPADDRESS=$(aws ec2 describe-instances  --instance-ids ${INSTANCE_ID}  --output table | grep PrivateIpAddress | head -n 1 | awk '{print $4}')
 
 sed -e "s/COMPONENT/${NAME}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id Z07578712H75FS9NNU2HC --change-batch file:///tmp/record.json &>/dev/null
+aws route53 change-resource-record-sets --hosted-zone-id Z10462863JH70KFPMLMA0 --change-batch file:///tmp/record.json &>/dev/null
 echo DNS Record Created
