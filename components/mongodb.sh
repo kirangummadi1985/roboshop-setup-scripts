42be74bbd4a818cba186162e9688958e8aea0123
kiran <kirangummadi1985@gmail.com>
2022-05-06 19:07:56 +0530

    Updating roboshop-setup-scripts

---
+++ components/mongodb.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+source components/common.sh
+checkRootUser
+
+ECHO "Setup MongoDB Yum repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Installing MongoDB"
+yum install -y mongodb-org  &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Configure Listen Address in MonogBD Configuration"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+statusCheck $?
+
+ECHO "Start MongoDB Service"
+systemctl restart mongod &>>${LOG_FILE} && systemctl enable mongod >>${LOG_FILE}
+statusCheck $?
+
+ECHO "Download Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"  >>${LOG_FILE}
+statusCheck $?
+
+ECHO "Extract Schema Zip"
+cd /tmp && unzip -o mongodb.zip >>${LOG_FILE}
+statusCheck $?
+
+cd mongodb-main
+ECHO "Load Schema"
+for component in catalogue users ; do
+mongo < ${component}.js >>${LOG_FILE}
+done
+statusCheck $?
